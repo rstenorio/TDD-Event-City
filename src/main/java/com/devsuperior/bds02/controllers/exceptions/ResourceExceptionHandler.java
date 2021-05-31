d142ebb5ac5de361e42c3e1ce5b56ddadaa4721a
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-05-31 11:59:41 +0200

    TAREFA: TDD Event-City

---
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.DBException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request ){
+		StandardError err = new StandardError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource Not Found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+	@ExceptionHandler(DBException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request ){
+		StandardError err = new StandardError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Resource Not Found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		
+	}
+	
+}
+
+
---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/DBException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class DBException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DBException(String message) {
+		super(message);
+	}
+	
+}
